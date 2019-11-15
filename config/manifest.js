703fe1caf8eae15f7cf42f3a0bd26227fb993f49
Arnaud S'Jongers <arnaud.sjongers@tenforce.com>
2019-11-15 14:31:14 +0100

    Initial setup for PWA with Mirage

---
+++ config/manifest.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = function(/* environment, appConfig */) {
+  // See https://zonkyio.github.io/ember-web-app for a list of
+  // supported properties
+
+  return {
+    name: "tourism-application",
+    short_name: "tourism-application",
+    description: "",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#fff",
+    theme_color: "#fff",
+    icons: [
+    ],
+    ms: {
+      tileColor: '#fff'
+    }
+  };
+}
