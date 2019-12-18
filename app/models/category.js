b859d7cdcbbc60687edea471c5db88d858f2bd84
Arnaud S'Jongers <arnaud.sjongers@tenforce.com>
2019-12-18 14:40:30 +0100

    Add icons for categories

--- app/models/category.js
+++ app/models/category.js
@@ -1,7 +1,20 @@
+import { computed } from '@ember/object';
+import { alias } from '@ember/object/computed';
+import { inject as service } from '@ember/service';
 import DS from 'ember-data';
 const { Model, attr } = DS;
 
 export default Model.extend({
+  currentDataSubject: service(),
+  dataSubject: alias("currentDataSubject.currentDataSubject"),
   name: attr(),
+  icons: attr(),
+  preferred: computed("dataSubject.{id,categories.@each}", function() {
+    return this.dataSubject.categories.includes(this);
+  }),
+  icon: computed("preferred", function() {
+    return this.preferred ? this.icons.filled : this.icons.default;
+  }),
+
   selected: true
 });
