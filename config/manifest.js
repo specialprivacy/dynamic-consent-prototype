d6c44bff9434f711eff170180a008edd993f395a
Arnaud S'Jongers <arnaud.sjongers@tenforce.com>
2019-12-27 14:11:33 +0100

    Update configuration

--- config/manifest.js
+++ config/manifest.js
@@ -8,11 +8,17 @@ module.exports = function(/* environment, appConfig */) {
     name: "dynamic-consent",
     short_name: "dynamic-consent",
     description: "",
-    start_url: "/dynamic-consent",
+    start_url: "/dynamic-consent/",
+    scope: "/dynamic-consent/",
     display: "standalone",
     background_color: "#fff",
     theme_color: "#fff",
     icons: [
+      {
+        "src": "/dynamic-consent/assets/images/icon-192.png",
+        "type": "image/png",
+        "sizes": "192x192"
+      }
     ],
     ms: {
       tileColor: '#fff'
