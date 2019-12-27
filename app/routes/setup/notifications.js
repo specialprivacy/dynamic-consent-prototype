f71247b0b3187151b6f78e2d4ecd498e6ebe3bf5
Arnaud S'Jongers <arnaud.sjongers@tenforce.com>
2019-12-27 22:04:58 +0100

    Ensure notification mode is loaded somehow

--- app/routes/setup/notifications.js
+++ app/routes/setup/notifications.js
@@ -1,7 +1,13 @@
+import { inject as service } from '@ember/service';
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  currentDataSubject: service(),
+
   model() {
     return this.store.findAll("notification-mode");
+  },
+  afterModel() {
+    return this.store.findRecord("notification-mode", this.currentDataSubject.currentDataSubject.belongsTo("notificationMode").id())
   }
 });
